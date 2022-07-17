c242405f6e014ed268018123734c966dfee9adca
Axwaizee <83546692+Axwaizee@users.noreply.github.com>
2022-07-17 17:29:14 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let click = new Audio('media/click.mp3');
+
+
+
+boxes = document.getElementsByClassName('body')
+boxes = Array.from(boxes)
+boxes.forEach((box)=>{
+    box.addEventListener('mouseover', ()=>{
+        click.play()
+    })
+})
