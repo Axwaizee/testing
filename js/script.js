b261933f7fd43ece74124e3775febc4b67289f35
Axwaizee <83546692+Axwaizee@users.noreply.github.com>
2022-07-19 15:45:17 +0530

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let click = new Audio('media/click.mp3');
-
-boxes = document.getElementsByClassName('body')
-boxes = Array.from(boxes)
-boxes.forEach((box)=>{
-    box.addEventListener('mouseover', ()=>{
-        click.play()
-    })
-})
